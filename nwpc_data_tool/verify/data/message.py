7f07ba12bf0cb86ed72d59b942a01688462b7dbd
perillaroc <perillaroc@gmail.com>
2020-09-02 16:11:52 +0800

    :art: return a fake message with wind speed when parameter is wind.

--- nwpc_data_tool/verify/data/message.py
+++ nwpc_data_tool/verify/data/message.py
@@ -1,5 +1,8 @@
 from nwpc_data.grib.eccodes import load_message_from_file
 
+import numpy as np
+import eccodes
+
 
 def get_message(
         data_path,
@@ -8,11 +11,18 @@ def get_message(
         level_type="isobaricInhPa",
 ):
     if parameter == "wind":
-        message = get_wind_message(
+        u_message, v_message = get_wind_message(
             data_path=data_path,
             level=level,
             level_type=level_type,
         )
+        message = eccodes.codes_clone(u_message)
+        u_array = eccodes.codes_get_double_array(u_message, "values")
+        v_array = eccodes.codes_get_double_array(v_message, "values")
+        w_array = np.sqrt(u_array*u_array + v_array*v_array)
+        eccodes.codes_release(u_message)
+        eccodes.codes_release(v_message)
+        eccodes.codes_set_double_array(message, "values", w_array)
     else:
         message = load_message_from_file(
             file_path=data_path,
