2b840e5a085d45731b6bda71f7366b74cb9d8b5c
perillaroc <perillaroc@gmail.com>
2020-09-02 10:09:43 +0800

    :sparkles: add support for field without climate data.
    Field with no climate does not compute acc.

--- nwpc_data_tool/verify/plev/eccodes.py
+++ nwpc_data_tool/verify/plev/eccodes.py
@@ -24,8 +24,8 @@ from .index import (
 def calculate_plev_stats(
         forecast_array: np.ndarray,
         analysis_array: np.ndarray,
-        climate_array: np.ndarray,
-        domain: typing.List,
+        climate_array: np.ndarray or None = None,
+        domain: typing.List or typing.Tuple or None = None,
 ) -> pd.DataFrame:
     """
 
@@ -43,6 +43,8 @@ def calculate_plev_stats(
 
     """
     # 坐标网格
+    if domain is None:
+        domain = (-90, 90, 0, 360)
     lat = np.arange(90, -90 - 1.5, -1.5)
     lon = np.arange(0, 360, 1.5)
     llon, llat = np.meshgrid(lon, lat)
@@ -56,24 +58,36 @@ def calculate_plev_stats(
     # 提取子区域
     domain_forecast_array = forecast_array[start_j:end_j, start_i:end_i]
     domain_analysis_array = analysis_array[start_j:end_j, start_i:end_i]
-    domain_climate_array = climate_array[start_j:end_j, start_i:end_i]
+    if climate_array is not None:
+        domain_climate_array = climate_array[start_j:end_j, start_i:end_i]
+    else:
+        domain_climate_array = None
 
     latitudes = llat[start_j:end_j, start_i:end_i]
 
-    df = pd.DataFrame({
-        "rmse": [np.sqrt(mse(domain_forecast_array, domain_analysis_array, latitudes))],
-        "me": [me(domain_forecast_array, domain_analysis_array, latitudes)],
-        "mae": [mae(domain_forecast_array, domain_analysis_array, latitudes)],
-        "sd": [sd(domain_forecast_array, domain_analysis_array, latitudes)],
-        "rmsem": [rmsem(domain_forecast_array, domain_analysis_array, latitudes)],
-        "rmsep": [rmsep(domain_forecast_array, domain_analysis_array, latitudes)],
-        "acc": [acc(domain_forecast_array, domain_analysis_array, domain_climate_array, latitudes)],
-    })
+    index_rmse = np.sqrt(mse(domain_forecast_array, domain_analysis_array, latitudes))
+    index_me = me(domain_forecast_array, domain_analysis_array, latitudes)
+    index_mae = mae(domain_forecast_array, domain_analysis_array, latitudes)
+    index_sd = sd(domain_forecast_array, domain_analysis_array, latitudes)
+    index_rmsem = rmsem(domain_forecast_array, domain_analysis_array, latitudes)
+    index_rmsep = rmsep(domain_forecast_array, domain_analysis_array, latitudes)
+
+    index_dict = {
+        "rmse": [index_rmse],
+        "me": [index_me],
+        "mae": [index_mae],
+        "sd": [index_sd],
+        "rmsem": [index_rmsem],
+        "rmsep": [index_rmsep],
+    }
+
+    if domain_climate_array is not None:
+        index_acc = acc(domain_forecast_array, domain_analysis_array, domain_climate_array, latitudes)
+        index_dict["acc"] = [index_acc]
+
+    df = pd.DataFrame(index_dict)
 
     if (df["rmse"] > 1000.0).item():
         df = df[:] = -999
 
     return df
-
-
-
--- nwpc_data_tool/verify/plev/field.py
+++ nwpc_data_tool/verify/plev/field.py
@@ -22,38 +22,56 @@ from .index import (
 def calculate_plev_stats(
         forecast_field: xr.DataArray,
         analysis_field: xr.DataArray,
-        climate_field: xr.DataArray,
-        domain: typing.List,
+        climate_field: xr.DataArray or None = None,
+        domain: typing.List or typing.Tuple or None = None,
 ) -> pd.DataFrame:
-    climate_field["longitude"] = analysis_field.longitude
 
     # 提取子区域
-    domain_forecast_field = forecast_field.sel(
-        latitude=slice(*domain[1::-1]),
-        longitude=slice(*domain[2:]),
-    )
+    domain_forecast_field = forecast_field
+    if domain is not None:
+        domain_forecast_field = forecast_field.sel(
+            latitude=slice(*domain[1::-1]),
+            longitude=slice(*domain[2:]),
+        )
 
-    domain_analysis_field = analysis_field.sel(
-        latitude=slice(*domain[1::-1]),
-        longitude=slice(*domain[2:]),
-    )
+    domain_analysis_field = analysis_field
+    if domain is not None:
+        domain_analysis_field = analysis_field.sel(
+            latitude=slice(*domain[1::-1]),
+            longitude=slice(*domain[2:]),
+        )
 
-    domain_climate_field = climate_field.sel(
-        latitude=slice(*domain[1::-1]),
-        longitude=slice(*domain[2:]),
-    )
+    domain_climate_field = climate_field
+    if climate_field is not None and domain is not None:
+        climate_field["longitude"] = analysis_field.longitude
+        domain_climate_field = climate_field.sel(
+            latitude=slice(*domain[1::-1]),
+            longitude=slice(*domain[2:]),
+        )
 
-    latitudes = forecast_field.latitude * xr.ones_like(forecast_field)
+    latitudes = domain_forecast_field.latitude * xr.ones_like(domain_forecast_field)
 
-    df = pd.DataFrame({
-        "rmse": [np.sqrt(mse(domain_forecast_field, domain_analysis_field, latitudes)).values],
-        "me": [me(domain_forecast_field, domain_analysis_field, latitudes).values],
-        "mae": [mae(domain_forecast_field, domain_analysis_field, latitudes).values],
-        "sd": [sd(domain_forecast_field, domain_analysis_field, latitudes).values],
-        "rmsem": [rmsem(domain_forecast_field, domain_analysis_field, latitudes).values],
-        "rmsep": [rmsep(domain_forecast_field, domain_analysis_field, latitudes).values],
-        "acc": [acc(domain_forecast_field, domain_analysis_field, domain_climate_field, latitudes).values],
-    })
+    index_rmse = np.sqrt(mse(domain_forecast_field, domain_analysis_field, latitudes)).values
+    index_me = me(domain_forecast_field, domain_analysis_field, latitudes).values
+    index_mae = mae(domain_forecast_field, domain_analysis_field, latitudes).values
+    index_sd = sd(domain_forecast_field, domain_analysis_field, latitudes).values
+    index_rmsem = rmsem(domain_forecast_field, domain_analysis_field, latitudes).values
+    index_rmsep = rmsep(domain_forecast_field, domain_analysis_field, latitudes).values
+
+    index_dict = {
+        "rmse": [index_rmse],
+        "me": [index_me],
+        "mae": [index_mae],
+        "sd": [index_sd],
+        "rmsem": [index_rmsem],
+        "rmsep": [index_rmsep],
+    }
+
+    if domain_climate_field is not None:
+        index_acc = acc(domain_forecast_field, domain_analysis_field, domain_climate_field, latitudes).values
+        index_dict["acc"] = [index_acc]
+
+    df = pd.DataFrame(index_dict)
 
     if (df["rmse"] > 1000.0).item():
         df = df[:] = -999
