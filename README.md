42a9feff69359e5d1bdf6da076a485afc1ce7ebf
perillaroc <perillaroc@gmail.com>
2022-01-18 11:32:04 +0800

    :truck: change name in setup and README.

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
 # reki-data-tool
 
-使用 [nwpc-oper/reki](https://github.com/nwpc-oper/reki) 构建的数据处理工具集
+使用 [nwpc-oper/reki](https://github.com/nwpc-oper/reki) 开发的数据处理测试项目。
 
-个人项目
+包括：
+
+- `ml`：机器学习
+- `postprocess`: 产品后处理
+- `verify`：检验
 
 ## LICENSE
 
 Copyright &copy; 2020-2022, perillaroc at cemc-oper.
 
-`cemc-data-tool` is licensed under [GPL v3.0](LICENSE.md)
+`reki-data-tool` is licensed under [GPL v3.0](LICENSE.md)
--- setup.py
+++ setup.py
@@ -14,15 +14,15 @@ with codecs.open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='reki-tool',
+    name='reki-data-tool',
 
     version=version,
 
-    description='A data tool.',
+    description='A data tool using nwpc-oper/reki.',
     long_description=long_description,
     long_description_content_type='text/markdown',
 
-    url='https://github.com/perillaroc/cemc-data-tool',
+    url='https://github.com/perillaroc/reki-data-tool',
 
     author='perillaroc',
     author_email='perillaroc@gmail.com',
@@ -33,7 +33,6 @@ setup(
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
