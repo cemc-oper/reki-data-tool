3a69de9523c8e265f8b0dcc72c72e0503c08dbb7
perillaroc <perillaroc@gmail.com>
2023-03-03 09:44:21 +0800

    :sparkles: add CLI for CMA-GFS wxzx data.

---
+++ reki_data_tool/postprocess/grid/gfs/wxzx/__main__.py
@@ -0,0 +1,69 @@
+from pathlib import Path
+from typing import Optional
+
+import typer
+
+from reki.data_finder import find_local_file
+from reki_data_tool.postprocess.grid.gfs.util import parse_time_options
+
+app = typer.Typer()
+
+
+@app.command()
+def serial(
+        start_time: Optional[str] = None,
+        forecast_time: Optional[str] = None,
+        input_file_path: Optional[Path] = None,
+        output_file_path: Optional[Path] = typer.Option(None)
+):
+    from reki_data_tool.postprocess.grid.gfs.wxzx.task_serial import make_wxzx_data_serial
+
+    if input_file_path is None:
+        start_time, forecast_time = parse_time_options(start_time, forecast_time)
+        input_file_path = find_local_file(
+            "grapes_gfs_gmf/grib2/orig",
+            start_time=start_time,
+            forecast_time=forecast_time
+        )
+
+    if input_file_path is None:
+        print("input file path is empty. Please set some options.")
+        raise typer.Exit(code=2)
+
+    make_wxzx_data_serial(
+        input_file_path=input_file_path,
+        output_file_path=output_file_path,
+    )
+
+
+@app.command()
+def dask_v1(
+        start_time: Optional[str] = None,
+        forecast_time: Optional[str] = None,
+        input_file_path: Optional[Path] = None,
+        output_file_path: Optional[Path] = typer.Option(...),
+        engine: str = "local",
+):
+    from reki_data_tool.postprocess.grid.gfs.wxzx.task_dask_v1 import make_wxzx_data_by_dask_v1
+
+    if input_file_path is None:
+        start_time, forecast_time = parse_time_options(start_time, forecast_time)
+        input_file_path = find_local_file(
+            "grapes_gfs_gmf/grib2/orig",
+            start_time=start_time,
+            forecast_time=forecast_time
+        )
+
+    if input_file_path is None:
+        print("input file path is empty. Please set some options.")
+        raise typer.Exit(code=2)
+
+    make_wxzx_data_by_dask_v1(
+        input_file_path=input_file_path,
+        output_file_path=output_file_path,
+        engine=engine
+    )
+
+
+if __name__ == "__main__":
+    app()
